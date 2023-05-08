9f9a0d52e3416e22b46d6e2276ec42192296afc7
shivani2190 <97497268+shivani2190@users.noreply.github.com>
2023-05-08 20:55:47 +0530

    Time: 404 ms (8.28%), Space: 54.4 MB (12.86%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -28,3 +28,46 @@ class Solution {
         quikcSort(j + 1, r);
     }
 }
+
+// public class Solution{
+//     public static void conquer(int arr[], int si, int mid, int ei){
+//         int merged[]= new int[ei-si+1];
+//         int idx = si;
+//         int idx = mid+1;
+//         int x=0;
+//         while(idx1 <=mid && idx2<=ei ){
+//             if(arr[idx1]<= arr[idx2]){
+//                 merged [x++]=arr[idx1++];
+//             }else{
+//                 merged[x++]=arr[idex2++];
+//             }
+//         }
+//         while(idx1<=mid){
+//             merged[x++]=arr[idx1++];
+//         }
+//         while(idx2<=ei){
+//             merged[x++]=arr[idx2++];
+//         }
+//         for(int i=0; j=si; i< merged.length ;i++;j++){
+//             arr[j] = merged[i];
+//         }
+//         public static void divide(int arr[], int si, int ei){
+//             if(si>=ei){
+//                 return;
+//             }
+//             int mid= si+(ei-si)/2;
+//             divide(arr,si,mid);
+//             divide(arr,mid+1,ei);
+//             conquer(arr,si,mid,ei);
+//         }
+//         public static void main(String args[]){
+//             int arr[] = {5,2,3,1};
+//             int n = arr.length;
+//             divide(arr, 0,n-1);
+//             for(int i=0;i<n;i++){
+//                 System.out.print(arr[i] + " ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }

c6986465860cd277c978f1c11124117acfe7ed81
shivani2190 <97497268+shivani2190@users.noreply.github.com>
2023-05-14 18:00:38 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (81.20%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -1,12 +1,24 @@
-class Solution {
-    public int[] shuffle(int[] nums, int n) {
-        int[] ans = new int[2*n];
-        int j=0;
-        for(int i =0; i<2*n;i=i+2){
-            ans[i]= nums[j];
-            ans[i+1] = nums[n+j];
-            j++;
+// class Solution {
+//     public int[] shuffle(int[] nums, int n) {
+//         int[] ans = new int[2*n];
+//         int j=0;
+//         for(int i =0; i<2*n;i=i+2){
+//             ans[i]= nums[j];
+//             ans[i+1] = nums[n+j];
+//             j++;
+//         }
+//         return ans;
+//     }
+// }
+class Solution{
+    public int[] shuffle(int[] nums, int n){
+        for(int i=0;i<n;i++){
+            nums[i+n] += nums[i]*1005;
         }
-        return ans;
+        for(int i=0;i<n; i++){
+            nums[2*i]= nums[i+n]/1005;
+            nums[2*i+1] = nums[i+n]%1005;
+        }
+        return nums;
     }
 }

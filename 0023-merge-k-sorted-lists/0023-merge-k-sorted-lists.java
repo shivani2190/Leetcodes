54e3e3900695f2ebdd23b05e24e7197cd994681d
shivani2190 <97497268+shivani2190@users.noreply.github.com>
2023-06-04 11:23:37 +0530

    Time: 8 ms (33.55%), Space: 44.6 MB (30.48%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -8,45 +8,65 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
-    public ListNode mergeKLists(ListNode[] lists) {
-        if(lists== null || lists.length == 0){
-            return null;
-        }
-        return mergeKLists(lists, 0, lists.length -1);
-    }
-    private ListNode mergeKLists(ListNode[] lists, int start, int end){
-        if(start==end){
-            return lists[start];
-        }
-        int mid = start + (end-start)/2;
-        ListNode left = mergeKLists(lists, start, mid);
-        ListNode right= mergeKLists(lists, mid+1, end);
-        return merge(left, right);
-    }
-    private ListNode merge(ListNode left,ListNode right){
-        ListNode head = new ListNode(-1);
+// class Solution {
+//     public ListNode mergeKLists(ListNode[] lists) {
+//         if(lists== null || lists.length == 0){
+//             return null;
+//         }
+//         return mergeKLists(lists, 0, lists.length -1);
+//     }
+//     private ListNode mergeKLists(ListNode[] lists, int start, int end){
+//         if(start==end){
+//             return lists[start];
+//         }
+//         int mid = start + (end-start)/2;
+//         ListNode left = mergeKLists(lists, start, mid);
+//         ListNode right= mergeKLists(lists, mid+1, end);
+//         return merge(left, right);
+//     }
+//     private ListNode merge(ListNode left,ListNode right){
+//         ListNode head = new ListNode(-1);
+//         ListNode temp = head;
+//         while(left != null && right!= null){
+//             if(left.val<right.val){
+//                 temp.next = left;
+//                 left=left.next;
+//             }else{
+//                 temp.next = right;
+//                 right = right.next;
+//             }
+//             temp=temp.next;
+//         }
+//         while(left!=null){
+//             temp.next=left;
+//             left=left.next;
+//             temp=temp.next;
+//         }
+//         while(right!=null){
+//             temp.next=right;
+//             right=right.next;
+//             temp=temp.next;
+//         }
+//         return head.next;
+//     }
+// }
+class Solution{
+    public ListNode mergeKLists(ListNode[] lists){
+        if(lists==null || lists.length==0) return null;
+        ListNode head = new ListNode(0);
         ListNode temp = head;
-        while(left != null && right!= null){
-            if(left.val<right.val){
-                temp.next = left;
-                left=left.next;
-            }else{
-                temp.next = right;
-                right = right.next;
+        List<Integer> l = new ArrayList<>();
+        for(ListNode list: lists){
+            while(list!=null){
+                l.add(list.val);
+                list=list.next;
             }
-            temp=temp.next;
         }
-        while(left!=null){
-            temp.next=left;
-            left=left.next;
-            temp=temp.next;
-        }
-        while(right!=null){
-            temp.next=right;
-            right=right.next;
+        Collections.sort(l);
+        for(int val: l){
+            temp.next= new ListNode(val);
             temp=temp.next;
         }
         return head.next;
-    }
+    } 
 }

b2bcb2a347ebf521b68e6c1355ecaf4362cd0afa
Shivani Singh <97497268+shivani2190@users.noreply.github.com>
2023-06-18 22:10:02 +0530

    Time: 0 ms (100.00%), Space: 39.8 MB (91.21%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -8,13 +8,13 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
-    public ListNode middleNode(ListNode head) {
-        ListNode slow = head, fast = head;
-        while (fast != null && fast.next != null) {
+class Solution{
+    public ListNode middleNode(ListNode head){
+        ListNode slow = head, fast= head;
+        while(fast !=null && fast.next != null){
             slow = slow.next;
-            fast = fast.next.next;
+            fast=fast.next.next;
         }
         return slow;
-    }
+    } 
 }

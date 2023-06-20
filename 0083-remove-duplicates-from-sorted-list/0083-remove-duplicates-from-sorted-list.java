f36322a2e1600845291873dfb55a2dd48dfcaa48
Shivani Singh <97497268+shivani2190@users.noreply.github.com>
2023-06-20 21:54:41 +0530

    Time: 0 ms (100.00%), Space: 43.3 MB (22.62%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
@@ -10,21 +10,35 @@
  */
 
 
+// class Solution{
+//     public ListNode deleteDuplicates(ListNode head){
+//         if(head==null || head.next==null) return head;
+//         ListNode prev = head;
+//         ListNode curr= head;
+//         while(curr!=null){
+//             if(prev.val==curr.val){
+//                 curr=curr.next;
+//             }else{
+//                 prev.next=curr;
+//                 prev=curr;
+//                 curr=prev.next;
+//             }
+//         }
+//         prev.next=null;
+//         return head;
+//     } 
+// }
 class Solution{
     public ListNode deleteDuplicates(ListNode head){
         if(head==null || head.next==null) return head;
-        ListNode prev = head;
-        ListNode curr = head.next;
-        while(curr!=null){
-            if(prev.val==curr.val){
-                curr=curr.next;
+        ListNode curr = head;
+        while(curr.next!=null){
+            if(curr.val==curr.next.val){
+                curr.next=curr.next.next;
             }else{
-                prev.next=curr;
-                prev=curr;
-                curr=prev.next;
+                curr=curr.next;
             }
         }
-        prev.next=null;
         return head;
     } 
 }

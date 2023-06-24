41b41228cf5039ce104234f41c26fbe75482d25f
Shivani Singh <97497268+shivani2190@users.noreply.github.com>
2023-06-24 14:45:20 +0530

    Time: 6 ms (28.18%), Space: 44.7 MB (5.23%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,11 +1,26 @@
-class Solution {
-    public int missingNumber(int[] nums) {
-        int n=nums.length;
-        int s1= n*(n+1)/2;
-        int s2=0;
-        for(int i=0;i<n;i++){
-            s2+=nums[i];
-        }
-        return s1-s2;
-    }
-}
+// class Solution {
+//     public int missingNumber(int[] nums) {
+//         int n=nums.length;
+//         int s1= n*(n+1)/2;
+//         int s2=0;
+//         for(int i=0;i<n;i++){
+//             s2+=nums[i];
+//         }
+//         return s1-s2;
+//     }
+// }
+ class Solution{
+     public int missingNumber(int[] nums){
+         Set<Integer> h= new HashSet<>();
+         for(int n : nums){
+             h.add(n);
+         }
+         int l=nums.length+1;
+         for(int i=0;i<l;i++){
+             if(!h.contains(i)){
+                 return i;
+             }
+         }
+         return -1;
+     }
+ }

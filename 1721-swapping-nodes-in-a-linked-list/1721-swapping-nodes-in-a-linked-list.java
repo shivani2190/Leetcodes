393037b2e38f4f193af59e8c9469ddef8c16683c
Shivani Singh <97497268+shivani2190@users.noreply.github.com>
2023-06-26 23:58:59 +0530

    Time: 2 ms (99.82%), Space: 56.8 MB (92.93%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
@@ -10,25 +10,20 @@
  */
 class Solution {
     public ListNode swapNodes(ListNode head, int k) {
-        ListNode tmp = head;
-        int ct = k;
-        ListNode res1 = head;
-        ListNode res2 = head;
-        while(ct>0){
-            res1 = tmp;
-            tmp = tmp.next;
-            ct--;
+        ListNode curr = head;
+        while(k>1){
+            curr=curr.next;
+            k--;
         }
-        // moving temporary pointer k times and the Kth node from the beginning pointer is moved (k-1) times.
-        while(tmp!=null){
-            tmp = tmp.next;
-            res2 = res2.next;
+        ListNode b = head;
+        ListNode a=curr;
+        while(curr.next!=null){
+            b=b.next;
+            curr=curr.next;
         }
-        // moving the temporary and res2 pointer simultaneously.
-        
-        int temp = res1.val;
-        res1.val = res2.val;
-        res2.val = temp;
+        int temp = a.val;
+        a.val=b.val;
+        b.val=temp;
         return head;
-        }
+}
 }

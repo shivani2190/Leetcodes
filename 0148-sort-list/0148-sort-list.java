718f13d95da706ca6422db936971bb4301af3dca
Shivani Singh <97497268+shivani2190@users.noreply.github.com>
2023-06-28 23:11:09 +0530

    Time: 11 ms (84.27%), Space: 56.2 MB (28.91%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -18,17 +18,17 @@ class Solution {
         ListNode temp=null;
         ListNode slow=head;
         ListNode fast = head;
-        while(fast!=null && fast.next!=null){
+        while(fast!=null && fast.next!=null){//4->2->1->3
             temp=slow;
             slow=slow.next;
             fast=fast.next.next;
         }
-        temp.next=null;
-        ListNode l1=sortList(head);
-        ListNode l2=sortList(slow);
-        return mergelist(l1,l2);
+        temp.next=null;//to make null of 2 in l1=4->2->null
+        ListNode l1=sortList(head);//head is pointing to 4
+        ListNode l2=sortList(slow);//slow is pointing to 1
+        return mergelist(l1,l2);//return 2->4 and 1->3
     }
-    ListNode mergelist(ListNode h1,ListNode h2){
+    ListNode mergelist(ListNode h1,ListNode h2){//to merge two sorted list like 2->4 and 1->3
         ListNode dummy = new ListNode();
         ListNode curr = dummy;
         while(h1!=null && h2!=null){
@@ -41,7 +41,7 @@ class Solution {
             }
             curr=curr.next;
         }
-        curr.next=h1!=null ? h1:h2;
+        curr.next=h1!=null ? h1:h2;//curr moves till both l1 and l2 is not null
         return dummy.next;
     }
     

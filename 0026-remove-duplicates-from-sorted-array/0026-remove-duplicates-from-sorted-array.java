fc25e03f166b60763d01ff769e024ff3f3620793
Shivani Singh <97497268+shivani2190@users.noreply.github.com>
2023-06-30 21:19:51 +0530

    Time: 1 ms (99.38%), Space: 43.5 MB (97.35%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -3,9 +3,11 @@ class Solution {
         int k = nums.length;
         int c=0;
         for(int i=0;i<k;i++){
+            // If the current element is equal to the next element, we skip
             if(i<k-1 && nums[i]==nums[i+1]){
                 continue;
             }
+            // We will update the array in place
             nums[c]=nums[i];
             c++;
         }

e7be03d52321d606b578d5bbbf2be387d75e3208
Shivani Singh <97497268+shivani2190@users.noreply.github.com>
2023-07-26 21:13:51 +0530

    Time: 2 ms (99.47%), Space: 43.7 MB (25.51%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -29,3 +29,16 @@ class Solution {
         return dummy.next;
     }
 }
+ // class Solution{
+ //     public ListNode addTwoNumbers(ListNode l1, ListNode l2){
+ //        return calculate(l1,l2,0); 
+ //     }
+ //     ListNode calculate(ListNode l1, ListNode l2, int carry){
+ //         if(l1 == null && l2== null && carry==0 ) return null;
+ //         if(l1 ==null && l2 ==null && carry!=0) return new ListNode(1);
+ //         int temp = (l1==null ? 0:l1.val) + (l2==null ? 0:l2.val) + carry;
+ //         ListNode res = new ListNode(temp%10);
+ //         res.next= calculate(l1==null ? null:l1.next, l1==null ? null:l2.next, temp/10);
+ //         return res;
+ //     }
+ // }

eea6f4ce6bd0e4c7b8e6b1ded11e062c11c67f4f
Shivani Singh <97497268+shivani2190@users.noreply.github.com>
2023-07-28 23:30:33 +0530

    Time: 1 ms (87.69%), Space: 39 MB (98.95%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
@@ -4,7 +4,7 @@ class Solution {
         int s=0;
         for(int i=1;i<=n;i++){
             s+=i;
-            if(s == (t - s + i)) return i;
+            if(s == (t - s + i)) return i;// s+i because i is two times for finding sum ie 1 to i then i to n
             }
         return -1;
         }

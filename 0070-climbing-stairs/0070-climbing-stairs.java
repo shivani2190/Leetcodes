843aeaf19d3353dc22b621c64e87cd273710dc66
Shivani Singh <97497268+shivani2190@users.noreply.github.com>
2023-07-31 21:43:58 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (52.36%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n==0) return 0;
+        if(n==1) return 1;
+        int one=1;
+        int two=0;
+        int result=0;
+        for(int i=1;i<=n;i++){
+            result=one+two;
+            two=one;
+            one=result;
+        }
+        return result;
+    }
+}

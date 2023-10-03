dc99e4d94520f0322474ea47396f827223108018
Shivani Singh <97497268+shivani2190@users.noreply.github.com>
2023-10-03 22:45:59 +0530

    Time: 1 ms (73.29%), Space: 40.3 MB (63.50%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.java
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.java
@@ -7,8 +7,6 @@ class Solution {
          for(int i=0; i<word2.length;i++){
             b=b+word2[i];
         }
-        System.out.println(a);
-        System.out.println(b);
         if(a.equals(b)){
             return true;
         }else{

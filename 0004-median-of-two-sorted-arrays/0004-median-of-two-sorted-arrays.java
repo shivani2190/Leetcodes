fd91a0494cf35c8ee21b5be47bda5bf14a912593
Shivani Singh <97497268+shivani2190@users.noreply.github.com>
2023-10-19 11:44:18 +0530

    Time: 5 ms (20.45%), Space: 45.2 MB (8.57%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,37 +1,112 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int m = nums1.length;
-        int n = nums2.length;
-        int[] a = new int[m+n];
-        int i=0,j=0,k=0;
-        while(i<m&&j<n){
-            if(nums1[i]<nums2[j]){
-                a[k]=nums1[i];
-                i++;
+        int a = nums1.length;
+        int b = nums2.length;
+        int j=0;
+        int[] arr = new int[a+b];
+        for(int i=0; i<arr.length;i++){
+            if(i<a){
+                arr[i]=nums1[i];
             }else{
-                a[k]=nums2[j];
-                j++;
-            }
-            k++;
-        }   
-        //again looping for the left elements in the array after being sorted
-            while(i<m){
-                a[k]=nums1[i];
-                i++;
-                k++;
-            }
-            while(j<n){
-                a[k]=nums2[j];
-                j++;
-                k++;
-            }
-            if((m+n)%2!=0){
-                return a[(m+n)/2];
-            }else{
-                int x=(m+n)/2;
-                double p=a[x];
-                double q=a[x-1];
-                return (p+q)/2;
+                arr[i]=nums2[j++];
             }
         }
+        Arrays.sort(arr);
+        if(arr.length%2==0){
+            return (double)(arr[arr.length/2] + arr[(arr.length-1)/2])/2;
+        }else{
+            return (double)arr[arr.length/2];  
+        }
     }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+//         int m = nums1.length;
+//         int n = nums2.length;
+//         int[] a = new int[m+n];
+//         int i=0,j=0,k=0;
+//         while(i<m&&j<n){
+//             if(nums1[i]<nums2[j]){
+//                 a[k]=nums1[i];
+//                 i++;
+//             }else{
+//                 a[k]=nums2[j];
+//                 j++;
+//             }
+//             k++;
+//         }   
+//         //again looping for the left elements in the array after being sorted
+//             while(i<m){
+//                 a[k]=nums1[i];
+//                 i++;
+//                 k++;
+//             }
+//             while(j<n){
+//                 a[k]=nums2[j];
+//                 j++;
+//                 k++;
+//             }
+//             if((m+n)%2!=0){
+//                 return a[(m+n)/2];
+//             }else{
+//                 int x=(m+n)/2;
+//                 double p=a[x];
+//                 double q=a[x-1];
+//                 return (p+q)/2;
+//             }
+//         }
+//     }

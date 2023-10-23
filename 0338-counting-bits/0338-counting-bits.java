8bd2dabf87cc75d669faed2ee7116734373cbc2e
Shivani Singh <97497268+shivani2190@users.noreply.github.com>
2023-10-23 23:27:23 +0530

    Time: 2 ms (84.18%), Space: 46.4 MB (43.74%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,17 +1,17 @@
 class Solution {
     public int[] countBits(int n) {
-        int[] dp = new int[n+1];
-        dp[0] = 0;
+        int[] ans = new int[n+1];
+        ans[0] = 0;
         int nearest = 0;
         for (int k = 1; k <= n; k++) {
             if ((k & (k-1)) == 0) {
-                dp[k] = 1;
+                ans[k] = 1;
                 nearest = k;
             } else {
-                dp[k] = dp[k-nearest] + dp[nearest];
+                ans[k] = ans[k-nearest] + ans[nearest];
             }
         }
         
-        return dp;
+        return ans;
     }
 }

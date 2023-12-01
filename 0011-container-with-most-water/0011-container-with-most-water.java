ee7d5414c5d3eabf51370dd3b4d843952bf30894
Shivani Singh <97497268+shivani2190@users.noreply.github.com>
2023-12-01 17:14:38 +0530

    Time: 5 ms (47.02%), Space: 55.4 MB (64.49%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,23 +1,12 @@
 class Solution {
     public int maxArea(int[] height) {
-      // Maximum area will be stored in this variable
-        int maximumArea = Integer.MIN_VALUE;
-        // Left and right pointers
-        int left = 0;
-        int right = height.length - 1;
-        // Loop until left and right meet
-        while (left < right) {
-            // Shorter pole/vertical line
-            int shorterLine = Math.min(height[left], height[right]);
-            // Update maximum area if required
-            maximumArea = Math.max(maximumArea, shorterLine * (right - left));
-            // If there is a longer vertical line present
-            if (height[left] < height[right]) {
-                left++;
-            } else {
-                right--;
-            }
+        int max =0, left =0, right = height.length-1;
+        while(left<right){
+           int currentarea = Math.min(height[left],height[right])*(right-left);
+            max = Math.max(max,currentarea);
+            if(height[left]<height[right]) left++;
+            else right--;
         }
-        return maximumArea;  
+        return max;
     }
 }

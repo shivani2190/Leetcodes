858ea905e5db4f9450c5bc3395e557730ff2bd82
Shivani Singh <97497268+shivani2190@users.noreply.github.com>
2023-12-05 10:06:20 +0530

    Time: 12 ms (78.90%), Space: 43.9 MB (39.28%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,25 +1,19 @@
 class Solution {
     public int maxVowels(String s, int k) {
-        int t = 0, n = s.length();
-        for (int i = 0; i < k; ++i) {
-            if (isVowel(s.charAt(i))) {
-                ++t;
-            }
+        int c=0;
+        for(int i=0;i<k;i++){
+           if(vowel(s.charAt(i))) c++; 
         }
-        int ans = t;
-        for (int i = k; i < n; ++i) {
-            if (isVowel(s.charAt(i))) {
-                ++t;
-            }
-            if (isVowel(s.charAt(i - k))) {
-                --t;
-            }
-            ans = Math.max(ans, t);
+        int ans = c;
+        for(int i=k;i<s.length();i++){
+           if(vowel(s.charAt(i))) c++;
+           if(vowel(s.charAt(i-k))) c--;
+            ans = Math.max(ans,c);
         }
         return ans;
     }
-
-    private boolean isVowel(char c) {
-        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+        static boolean vowel(char c){
+            if(c=='a' || c=='e'|| c=='i'|| c=='o'|| c=='u') return true;
+            return false;
     }
 }

40508c7fe8267b7fbbb3c11e225730f3decc3bf8
Shivani Singh <97497268+shivani2190@users.noreply.github.com>
2023-12-06 21:50:44 +0530

    Time: 9 ms (93.10%), Space: 44.3 MB (81.86%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.java
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.java
@@ -1,20 +1,16 @@
 class Solution {
     public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
         List<List<Integer>> ans = new ArrayList<>();
-        Set<Integer> s1=new HashSet<>();
-        Set<Integer> s2=new HashSet<>();
-        for(int i: nums1) s1.add(i);
-        for(int i:nums2) s2.add(i);
-        List<Integer> l1 = new ArrayList<>();
-        List<Integer> l2 = new ArrayList<>();
-        for(int i : s1){
-            if(!s2.contains(i)) l1.add(i);
-        }
-        for(int i :s2){
-            if(!s1.contains(i)) l2.add(i);
-        }
-        ans.add(l1);
-        ans.add(l2);
-        return ans;  
+        List<Integer> a = new ArrayList<>();
+        List<Integer> b = new ArrayList<>();
+        Set<Integer> s1 = new HashSet<>();
+        Set<Integer> s2 = new HashSet<>();
+        for(int i : nums1) s1.add(i);
+        for(int i : nums2) s2.add(i);
+        for(int i : s1) if(!s2.contains(i)) a.add(i);
+        for(int i : s2) if(!s1.contains(i)) b.add(i);
+        ans.add(a);
+        ans.add(b);
+        return ans;
     }
 }

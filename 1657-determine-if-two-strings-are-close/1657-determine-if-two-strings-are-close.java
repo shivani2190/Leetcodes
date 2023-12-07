f68e3a47ab28ec627bf904a6767006f6722224bd
Shivani Singh <97497268+shivani2190@users.noreply.github.com>
2023-12-07 17:44:45 +0530

    Time: 65 ms (42.25%), Space: 44.7 MB (42.98%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
@@ -1,22 +1,19 @@
 class Solution {
     public boolean closeStrings(String word1, String word2) {
-         if (word1.length() != word2.length())
-            return false;
-        int[] counts1 = new int[26];
-        int[] counts2 = new int[26];
-        int length = word1.length();
-        for (int i = 0; i < length; i++) {
-            char c1 = word1.charAt(i), c2 = word2.charAt(i);
-            counts1[c1 - 'a']++;
-            counts2[c2 - 'a']++;
+        if(word1.length() != word2.length()) return false;
+       Map<Character, Integer> m = new HashMap<>();
+        Map<Character, Integer> n = new HashMap<>();
+        for(char num : word1.toCharArray()){
+            m.put(num , m.getOrDefault(num,0)+1);
         }
-        for (int i = 0; i < 26; i++) {
-            if (counts1[i] == 0 ^ counts2[i] == 0)
-                return false;
+        for(char num : word2.toCharArray()){
+            n.put(num , n.getOrDefault(num,0)+1);
         }
-        Arrays.sort(counts1);
-        Arrays.sort(counts2);
-        return Arrays.equals(counts1, counts2);
-        
+        if(!m.keySet().equals(n.keySet())) return false;
+        List<Integer> a = new ArrayList<>(m.values());
+        List<Integer> b = new ArrayList<>(n.values());
+        Collections.sort(a);
+        Collections.sort(b);
+        return a.equals(b);
     }
 }

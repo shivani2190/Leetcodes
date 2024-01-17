1e733fcc7781492b6ee0e8d746f35a2375498069
Shivani Singh <97497268+shivani2190@users.noreply.github.com>
2024-01-17 12:32:05 +0530

    Time: 2 ms (98.10%), Space: 41.7 MB (21.99%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -1,8 +1,8 @@
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
-       HashMap<Integer, Integer> h = new HashMap();
+       HashMap<Integer, Integer> h = new HashMap<>();
         for(int num: arr){
-            h.put(num, h.getOrDefault(num, 0)+1);
+            h.put(num, h.getOrDefault(num, 0)+1);//h.get is used to add occurrences but h.getOrDefault gives default 0 if no occurrence else +1 if present
         }
         HashSet<Integer> s = new HashSet<>(h.values());
         return h.size() == s.size();

f75260644940a1553808c37cf85101460864d85e
Shivani Singh <97497268+shivani2190@users.noreply.github.com>
2024-03-06 17:45:29 +0530

    Time: 0 ms (100.00%), Space: 44.5 MB (41.99%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -9,35 +9,34 @@
  *     }
  * }
  */
-// public class Solution {
-//     public boolean hasCycle(ListNode head) {
-//         Set<ListNode> l= new HashSet<>();//creating hashset accepting listnode parameter
-//         while(head!=null){
-//             if(l.contains(head)){
-//                 return true;
-//             }else{
-//                 l.add(head);
-//             }
-//             head=head.next;
-//         }
-//         return false;
-//     }
-// }
-
-public class Solution{
-    public boolean hasCycle(ListNode head){
-        if(head==null){
-            return false;
-        }
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head==null) return false;
         ListNode slow = head;
         ListNode fast = head.next;
         while(slow!=fast){
             if(fast==null || fast.next==null){
                 return false;
             }
-            slow = slow.next;
+            slow=slow.next;
             fast = fast.next.next;
         }
         return true;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
